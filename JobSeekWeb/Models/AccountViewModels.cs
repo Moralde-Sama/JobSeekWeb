429d69c10eb1354fe821a8a1b9561635f5e38eb2
Moralde-Sama <moralde.sama@gmail.com>
2019-06-23 21:48:49 +0800

    Migrated amad landing page design and added save user by its type

--- JobSeekWeb/Models/AccountViewModels.cs
+++ JobSeekWeb/Models/AccountViewModels.cs
@@ -79,6 +79,7 @@ namespace JobSeekWeb.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+        public bool IsWorker { get; set; }
     }
 
     public class ResetPasswordViewModel
--- JobSeekWeb/Models/Job.Context.cs
+++ JobSeekWeb/Models/Job.Context.cs
@@ -33,15 +33,15 @@ namespace JobSeekWeb.Models
         public virtual DbSet<tbl_asp_users> tbl_asp_users { get; set; }
         public virtual DbSet<tbl_chat> tbl_chat { get; set; }
         public virtual DbSet<tbl_chat_attachment> tbl_chat_attachment { get; set; }
-        public virtual DbSet<tbl_company> tbl_company { get; set; }
         public virtual DbSet<tbl_company_job> tbl_company_job { get; set; }
         public virtual DbSet<tbl_notification> tbl_notification { get; set; }
         public virtual DbSet<tbl_project> tbl_project { get; set; }
         public virtual DbSet<tbl_project_participants> tbl_project_participants { get; set; }
         public virtual DbSet<tbl_requirements> tbl_requirements { get; set; }
         public virtual DbSet<tbl_skill> tbl_skill { get; set; }
-        public virtual DbSet<tbl_worker> tbl_worker { get; set; }
         public virtual DbSet<tbl_worker_skill> tbl_worker_skill { get; set; }
         public virtual DbSet<tbl_category> tbl_category { get; set; }
+        public virtual DbSet<tbl_company> tbl_company { get; set; }
+        public virtual DbSet<tbl_worker> tbl_worker { get; set; }
     }
 }
