faf445a678a44c9a0ea449b41c309486d6ba69e8
ConquerThroughConnectivity <jaycemico@gmail.com>
2019-06-25 18:08:46 +0800

    Landing page partial, then amad paki check ko atong nav color dili makita

---
+++ JobSeekWeb/Controllers/CompanyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobSeekWeb.Controllers
+{
+    public class CompanyController : Controller
+    {
+        // GET: Company
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult LandingPage()
+        {
+            return View();
+        }
+
+    }
+}
