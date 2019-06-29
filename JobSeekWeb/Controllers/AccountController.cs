ffd29cf679187bd6d342e3fdcb38a655e0fd6dd6
Moralde-Sama <moralde.sama@gmail.com>
2019-06-29 21:52:18 +0800

    changed login process, use username to login instead of email

--- JobSeekWeb/Controllers/AccountController.cs
+++ JobSeekWeb/Controllers/AccountController.cs
@@ -123,7 +123,7 @@ namespace JobSeekWeb.Controllers
             }
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
